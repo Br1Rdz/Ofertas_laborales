fc331aff2a9d445e28d8790bcf6a970a0ad3960e
Br1Rdz <91506187+Br1Rdz@users.noreply.github.com>
2025-06-20 10:13:48 -0600

    Update 3_📑_Concluciones.py

--- pages/3_📑_Concluciones.py
+++ pages/3_📑_Concluciones.py
@@ -58,45 +58,4 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-
-# markdown_concluciones = ''' 
-# * Durante los meses de abril y mayo de 2025 se recopilaron 530 ofertas laborales dirigidas a biólogos.
-# Sin embargo, 297 de estas no reportaron información salarial o presentaban valores atípicos,
-# por lo que solo 233 ofertas fueron consideradas para el análisis detallado.
-
-# * La cobertura geográfica de las ofertas fue desigual. Se identificaron al menos una oferta en 22 de las 32 entidades federativas,
-# mientras que las 10 entidades restantes no registraron ninguna vacante durante el periodo evaluado.
-
-# * Los sueldos ofrecidos para biólogos oscilaron entre :orange[\$6,000 y \$22,000 pesos mensuales].
-# La entidad con los sueldos más altos reportados fue Nuevo León, lo que sugiere una mejor remuneración relativa en dicha región.
-
-# * El análisis de palabras clave y sueldos promedio permitió identificar tres grupos.
-# Términos como “químico”, “farmacéutico” y “laboratorio” (asociados a laboratorio) 
-# tiene rango de sueldos promedio entre :orange[\$12,000 y \$15,000 pesos mensuales].
-# No obstante, se reconoce la necesidad de refinar el análisis semántico,
-# ya que algunas palabras incluidas podrían no tener una relevancia significativa.
-
-# * El Estado de México (México) fue la entidad federativa con el mayor número de vacantes,
-# registrando un total de 70 ofertas. Sin embargo, esta entidad también presentó una discrepancia
-# entre el sueldo promedio ofrecido y el ingreso necesario mensual estimado por ENSAFI (2024),
-# con una diferencia de :red[\$5,141 pesos].
-
-# * Se identificó una discrepancia entre los sueldos promedio ofrecidos y el ingreso mensual necesario estimado por ENSAFI (2024)
-# en :orange[18 de las 22 entidades federativas evaluadas].
-# La Ciudad de México presentó la diferencia más pronunciada entre ambos valores.
-
-# * La categoría laboral mejor representada fue la de “laboratorio”, que concentró 194 ofertas con sueldos
-# que variaron entre :orange[\$6,000 y \$22,000 pesos]. Esta categoría no solo fue la más frecuente,
-# sino también la que mostró una mayor amplitud en el rango salarial,
-# lo que refleja la diversidad de responsabilidades dentro del área.
-
-# * Las ofertas analizadas provienen de los meses de :red[abril y mayo del 2025 de las plataformas OCC y Computrabajo],
-# lo cual podria limitar la representatividad del análisis. Aun así, se observa una tendencia clara hacia las actividades técnicas
-# de laboratorio. Este hallazgo sugiere que los estudiantes de biología que deseen insertarse en el mercado laboral deberían priorizar
-# la adquisición de habilidades prácticas y técnicas especializadas en dichas áreas.
-# No obstante, la amplia variabilidad en los sueldos refleja que estas habilidades pueden estar asociadas tanto
-# a tareas operativas básicas como a responsabilidades de gestión,
-# lo que refuerza la importancia de una formación integral y avanzada.
-# '''
-
-# st.markdown(markdown_concluciones, unsafe_allow_html=True) 
+st.markdown("<h1 style='text-align: center; font-size: 25px; color: white;'>✨Gracias por tu apoyo✨</h1>", unsafe_allow_html=True)
